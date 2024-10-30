3e96ea31f0731dc564cb17b5314846cff5dde63e
zimboz25 <joshburgo25@gmail.com>
2024-10-30 16:00:43 +1000

    Updated index.html code to pull events from database

--- website/views.py
+++ website/views.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, render_template
+from .models import Event
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
-    return render_template('index.html')  # Renders index.html as the landing page
+    # Query all events from the database
+    events = Event.query.all()
+    # Render the 'index.html' template with the events data
+    return render_template('index.html', events=events)
